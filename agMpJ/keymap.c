8dc2425f13c5c4deb190854b13301d3b2668c513
Evan Moelter <2207348+evanmoelter@users.noreply.github.com>
2025-04-22 19:10:54 +0200

    Add handedness mapping for chordal hold

--- agMpJ/keymap.c
+++ agMpJ/keymap.c
@@ -10,7 +10,14 @@ enum custom_keycodes {
   HSV_169_255_255,
 };
 
-
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+  LAYOUT_voyager(
+    'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+    'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+    'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+    'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+                        '*', '*',  '*', '*'
+  );
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
